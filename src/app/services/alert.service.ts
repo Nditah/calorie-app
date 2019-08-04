cd4b796a76c6d744b44cb504345d5d9beb60514d
Mohammed Odunayo <factman60@gmail.com>
2019-08-04 01:05:49 +0100

    Android Fixed

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
 
-  constructor(private toastController: ToastController) { }
+  constructor(private toastController: ToastController, private alertController: AlertController) { }
 
   async presentToast(message: any) {
     const toast = await this.toastController.create({
@@ -16,5 +16,23 @@ export class AlertService {
       color: 'dark'
     });
     toast.present();
+  }
+
+  public async alert(header: string, message: string, cancel: () => void, okay: () => void) {
+    const alert = await this.alertController.create({ header, message,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: cancel
+        }, {
+          text: 'Okay',
+          handler: okay
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
